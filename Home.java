ff89c343f810d70252c632f96ba860e196153c4e
syyyl <56541363+syyyl@users.noreply.github.com>
2024-01-13 18:01:48 +0900

    Update Home.java

--- Home.java
+++ Home.java
@@ -1,4 +1,36 @@
 package com.ohgiraffers.section00.hw;
 
+import java.util.Random;
+
 public class Home extends Place {
+
+    public Home() {
+
+        double min = 0.0;
+        double max = 0.6;
+        double range = max - min;
+
+        public void search() {
+            if (randomAct(range)) {
+                System.out.println("탐색 성공");
+            } else {
+                System.out.println("탐색 실패");
+            }
+        }
+
+        public void sleep() {
+            if (randomAct(range)) {
+                System.out.println("휴식 성공");
+            } else {
+                System.out.println("휴식 실패");
+            }
+        }
+
+
+
+        }
+
+
 }
+
+
