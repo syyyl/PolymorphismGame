06b4caa8ba56460a1f13e061af93adb83c51e7eb
syyyl <56541363+syyyl@users.noreply.github.com>
2024-01-14 05:22:59 +0900

    Update Apartment.java

--- Apartment.java
+++ Apartment.java
@@ -1,31 +1,39 @@
 package com.ohgiraffers.section00.hw;
 
+import java.util.Random;
+
 public class Apartment extends Place {
 
-    public Apartment() {
+    public Apartment() {}
 
-        double min = 0.0;
-        double max = 0.6;
-        double range = max - min;
+    Random random = new Random();
 
-        public void search() {
-            if (randomAct(range)) {
-                System.out.println("탐색 성공");
-            } else {
-                System.out.println("탐색 실패");
-            }
-        }
-
-        public void sleep() {
-            if (randomAct(range)) {
-                System.out.println("휴식 성공");
-            } else {
-                System.out.println("휴식 실패");
-            }
-        }
-
-
-
+    public boolean randomAct (double probability) {
+        double randomValue = random.nextDouble();
+        return randomValue <= probability;
     }
+
+
+    public void search(Game game) {
+        if (randomAct(0.8)) {
+            System.out.println("필요한 물건을 발견했습니다! Hp +10");
+            game.changedHp(10);
+        } else {
+            System.out.println("아무것도 찾지 못했습니다.");
+        }
+    }
+
+    public void sleep(Game game) {
+        if (randomAct(0.2)) {
+            System.out.println("충분한 휴식을 취했습니다. Hp +10");
+            game.changedHp(10);
+        } else {
+            System.out.println("밤 사이에 습격을 받았습니다. Hp -10");
+            game.changedHp(-10);
+        }
+    }
+
+
+
 }
 
