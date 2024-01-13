90a7def4f3b0e813a2a619920987ccb612b3d024
syyyl <56541363+syyyl@users.noreply.github.com>
2024-01-13 18:01:35 +0900

    Update Place.java

--- Place.java
+++ Place.java
@@ -1,13 +1,43 @@
 package com.ohgiraffers.section00.hw;
 
+import java.util.Random;
+
 public class Place {
 
-    public void search() {
+    public Place() {}
 
+    int searchP;
+    int sleepP;
+
+
+    Random random = new Random();
+    double min = 0.0;
+    double max = 1.0;
+    double range = max - min;
+
+    public boolean randomAct (double probability) {
+
+        double randomValue = random.nextDouble() * range;
+        return randomValue <= probability;
+    }
+
+
+    public void search() {
+        if (randomAct(range)) {
+            System.out.println("탐색 성공");
+        } else {
+            System.out.println("탐색 실패");
+        }
     }
 
     public void sleep() {
-
+        if (randomAct(range)) {
+            System.out.println("휴식 성공");
+        } else {
+            System.out.println("휴식 실패");
+        }
     }
+
+
 
 }
