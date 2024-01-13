3f4949cd1d9ad424ef3eedcee23e9fa1a5a3d594
syyyl <56541363+syyyl@users.noreply.github.com>
2024-01-13 18:02:17 +0900

    Update School.java

--- School.java
+++ School.java
@@ -1,4 +1,30 @@
 package com.ohgiraffers.section00.hw;
 
 public class School extends Place {
+
+    public School() {
+
+        double min = 0.0;
+        double max = 0.6;
+        double range = max - min;
+
+        public void search() {
+            if (randomAct(range)) {
+                System.out.println("탐색 성공");
+            } else {
+                System.out.println("탐색 실패");
+            }
+        }
+
+        public void sleep() {
+            if (randomAct(range)) {
+                System.out.println("휴식 성공");
+            } else {
+                System.out.println("휴식 실패");
+            }
+        }
+
+
+
+    }
 }
