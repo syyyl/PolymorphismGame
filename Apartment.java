eb29227d754ce96e439ebdfb3a91994f18d83a4d
syyyl <56541363+syyyl@users.noreply.github.com>
2024-01-13 18:02:05 +0900

    Update Apartment.java

--- Apartment.java
+++ Apartment.java
@@ -1,4 +1,31 @@
 package com.ohgiraffers.section00.hw;
 
 public class Apartment extends Place {
+
+    public Apartment() {
+
+        double min = 0.0;
+        double max = 0.6;
+        double range = max - min;
+
+        public void search() {
+            if (randomAct(range)) {
+                System.out.println("탐색 성공");
+            } else {
+                System.out.println("탐색 실패");
+            }
+        }
+
+        public void sleep() {
+            if (randomAct(range)) {
+                System.out.println("휴식 성공");
+            } else {
+                System.out.println("휴식 실패");
+            }
+        }
+
+
+
+    }
 }
+
